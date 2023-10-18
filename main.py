8d5cfe09f8e087889f7772fbccd96ae1f500dc8f
Bruno Caldara <bruno@localhost.localdomain>
2023-10-18 14:21:19 -0300

    Cria tela de ultimos resultados

--- main.py
+++ main.py
@@ -4,12 +4,66 @@ from dotenv import load_dotenv
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.textfield import MDTextField
 
 load_dotenv()
 
 
 class MenuScreen(MDScreen):
     pass
+
+
+class UltimoResultadoScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super(UltimoResultadoScreen, self).__init__(*args, **kwargs)
+        self.API_BASE_URL = MDApp.get_running_app().API_BASE_URL
+        self.API_VERSION = MDApp.get_running_app().API_VERSION
+        self.API_JWT_TOKEN = MDApp.get_running_app().API_JWT_TOKEN
+        self.primary_color = MDApp.get_running_app().primary_color
+        self.secundary_color = MDApp.get_running_app().secundary_color
+
+    def on_pre_enter(self):
+        # Forçar pintar os inputs
+        for key in self.ids.keys():
+            if isinstance(self.ids[key], MDTextField):
+                self.ids[key].focus = True
+                self.ids[key].focus = False
+                self.ids[key].bind(focus=self.on_textfield_focus)
+
+    def on_enter(self):
+        self.preenche_ultimo_sorteio()
+
+    def get_ultimo_sorteio(self):
+        req = requests.get(f'{self.API_BASE_URL}/{self.API_VERSION}/megasena/resultados?sorteio=ultimo',
+                           headers={'Accept': 'application/json', 'Authorization': f'Bearer {self.API_JWT_TOKEN}'})
+        return req.json()[0], req.status_code
+
+    def preenche_ultimo_sorteio(self):
+        req_json, status_code = self.get_ultimo_sorteio()
+        if status_code == 200:
+            self.ids.txt_ultimo_resultado_sorteio.text = str(
+                req_json['sorteio'])
+            self.ids.txt_ultimo_resultado_data.text = str(req_json['data'])
+            self.ids.txt_ultimo_resultado_dezena1.text = str(
+                req_json['dezena1'])
+            self.ids.txt_ultimo_resultado_dezena2.text = str(
+                req_json['dezena2'])
+            self.ids.txt_ultimo_resultado_dezena3.text = str(
+                req_json['dezena3'])
+            self.ids.txt_ultimo_resultado_dezena4.text = str(
+                req_json['dezena4'])
+            self.ids.txt_ultimo_resultado_dezena5.text = str(
+                req_json['dezena5'])
+            self.ids.txt_ultimo_resultado_dezena6.text = str(
+                req_json['dezena6'])
+
+    def btn_ultimo_resultado_click(self):
+        self.preenche_ultimo_sorteio()
+
+    def on_textfield_focus(self, instance_text_field, focus: bool):
+        # pass
+        if instance_text_field.label_id is not '':
+            self.ids[instance_text_field.label_id].color = self.secundary_color if focus == True else self.primary_color
 
 
 class ResultadosScreen(MDScreen):
@@ -35,6 +89,7 @@ class LoteriaCefApp(MDApp):
         self.theme_cls.primary_palette = "Blue"
 
         sm = MDScreenManager()
+        sm.add_widget(UltimoResultadoScreen())
         sm.add_widget(MenuScreen())
         sm.add_widget(ResultadosScreen())
         sm.add_widget(ContagemNumerosScreen())
@@ -43,13 +98,13 @@ class LoteriaCefApp(MDApp):
 
         return sm
 
-    def bt_ultimo_resultado_click(self):
+    # def bt_ultimo_resultado_click(self):
+    #     print(self.API_BASE_URL)
+    #     print(self.API_JWT_TOKEN)
+
+    def bt_resultado_click(self):
         print(self.API_BASE_URL)
         print(self.API_JWT_TOKEN)
-        r = requests.get(f'{self.API_BASE_URL}/{self.API_VERSION}/megasena/resultados?sorteio=ultimo',
-                         headers={'Accept': 'application/json', 'Authorization': f'Bearer {self.API_JWT_TOKEN}'})
-        print(r.json())
-        print(f'status code: {r.status_code}')
 
 
 if __name__ == '__main__':
